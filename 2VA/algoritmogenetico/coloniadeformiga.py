a13abc5ddc0ec024922c9cfa6f60cfaf9de1fdf5
Paulo Batista <59277127+kattagarian@users.noreply.github.com>
2023-09-10 11:11:31 -0300

    2VA

---
+++ 2VA/algoritmogenetico/coloniadeformiga.py
@@ -0,0 +1,163 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math, random, time
+
+def lerEntrada():
+    arquivo = open('berlin10.tsp')    #substituir por matriztsp7.tsp para entradas pequenas e berlin52.tsp para entradas grandes
+    arquivo = arquivo.readlines()     #coloca cada linha em uma lista diferente
+    aux = coords = []
+    for i in range(6, len(arquivo)):    #começa o loop a partir da sexta linha onde começam as coordenadas
+        aux = list(map(float, arquivo[i].split()))  #divide a linha em 3, indice da cidade, x e y
+        coords.append([aux.pop(1), aux.pop(1)])     #remove o x e depois remove o y
+    return coords
+
+def matrizes(coords, qtdCidades):
+    #Iniciar a matriz 52x52 com zeros
+    matrizDistancia = np.zeros((qtdCidades, qtdCidades))
+    for i in range(qtdCidades):
+        for j in range(qtdCidades -1, i, -1):
+            #Calcular cada distancia euclidiana entre as cidades e colocar o resultado na matriz
+            matrizDistancia[i][j] = math.sqrt(((coords[i][0] - coords[j][0])**2) + ((coords[i][1] - coords[j][1])**2))
+            #espelhar a matriz para que as duas metades tenham mesmos valores
+            matrizDistancia[j][i] = matrizDistancia[i][j]
+    return matrizDistancia
+
+def plotIterDistancias(melhoresDistancias, alfa, beta, rho):
+    #plot das distâncias por geração
+    plt.figure(figsize=(10, 6))
+    plt.plot(melhoresDistancias, marker='o', linestyle='-', color='b')
+    plt.xlabel('Iteração')
+    plt.ylabel('Melhor distância')
+    plt.title(f'Melhores distâncias | alfa: {alfa}, beta: {beta}, rho: {rho}')
+    plt.grid(True)
+    #plt.show(block=False)
+    plt.savefig("plota{alfa}b{beta}rho{rho}f{formigas}i{iteracoes}.png")
+
+def plotCaminho(melhorRota_cidades, cidades, alfa, beta, rho):
+    #plot do melhor caminho
+    plt.figure(figsize=(10, 6))
+    plt.scatter(cidades[:, 0], cidades[:, 1], color='blue', label='Cidades')
+    plt.plot(melhorRota_cidades[:, 0], melhorRota_cidades[:, 1], color='red', linewidth=2, label='Rota')
+    for i, city in enumerate(melhorRota_cidades):
+        plt.text(city[0], city[1], str(i), fontsize=12, color='black')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.title(f'Melhor Solução: {melhorDistancia:.2f} | alfa: {alfa}, beta: {beta}, evaporação: {rho}, formigas: {formigas}, iteracoes: {iteracoes}')
+    plt.grid(True)
+    plt.savefig("plot2a{alfa}b{beta}rho{rho}f{formigas}i{iteracoes}.png")
+
+coords = lerEntrada()       #recebe a entrada
+cidades = np.array(coords)  #array em 2D com as coordenadas da cidade
+qtdCidades = len(coords)    #variável com a quantidade de cidades
+
+'''#HYPERPARAMETROS TESTE
+formigas = random.choice([10, 20, 30, 50, 100, 200])   #numero de formigas que percorrerão os caminhos a cada iteração default: 30
+iteracoes = random.choice([50, 100, 200, 300, 500]) #numero de iteracoes a serem executadas default: 200
+alfa = random.choice([1, 2, 3, 4, 5, 10, 15])     #quanto maior o alfa, mais o feromônio vai impactar default: 1
+beta = random.choice([1, 2, 3, 4, 5, 10, 15])            #quanto maior o beta, mais a distância vai impactar default: 5
+rho = random.choice([0, 0.001, 0.01, 0.05, 0.1, 0.2, 0.5])       #taxa de evaporação default: 0.5
+Q = 100         #uma constante relacionada à quantidade de trilha deixada pelas formigas default: 100'''
+
+#HIPERPARAMETROS BERLIN52
+formigas = 20  #numero de formigas que percorrerão os caminhos a cada iteração default: 30
+iteracoes = 50 #numero de iteracoes a serem executadas default: 200
+alfa = 1        #quanto maior o alfa, mais o feromônio vai impactar default: 1
+beta = 5        #quanto maior o beta, mais a distância vai impactar default: 5
+rho = 0.5         #taxa de evaporação default: 0.5
+Q = 100         #uma constante relacionada à quantidade de trilha deixada pelas formigas default: 100
+
+'''#HIPERPARAMETROS ENTRADAS PEQUENAS
+formigas = 10   #numero de formigas que percorrerão os caminhos a cada iteração default: 30
+iteracoes = 50  #numero de iteracoes a serem executadas default: 200
+alfa = 1        #quanto maior o alfa, mais o feromônio vai impactar default: 1
+beta = 5        #quanto maior o beta, mais a distância vai impactar default: 5
+rho = 0         #taxa de evaporação default: 0.5
+Q = 100         #uma constante relacionada à quantidade de trilha deixada pelas formigas default: 100'''
+
+matrizDistancia = matrizes(coords, len(coords))     #matriz com as distâncias entre as cidades
+matrizFeromonio = np.ones((qtdCidades, qtdCidades)) #matriz com os feromonios entre cada cidade.
+melhoresDistancias = []     #lista que vai guardar as melhores rotas de cada iteração
+melhoresRotas = []          #lista com as melhores rotas
+melhorRota = None           #inicializa variavel que vai armazenar a melhor rota global
+melhorDistancia = float('inf')  #inicializa variavel com a melhor distancia global
+passo = 0   #variavel pra calcular a porcentagem
+inicio = time.time()
+
+for i in range(iteracoes):
+    caminhoFormiga = [] #a cada iteração zera a lista com o caminho das formigas
+    caminhoFormigaDistancia = []    #zera as distancias percorridas pela formiga
+    
+    porcentagem = i/iteracoes   #função visual apenas para mostrar o progresso
+    if(porcentagem >= passo):
+        fim = time.time() - inicio
+        print(f'{porcentagem*100}% iteração = {i} - {fim:.3f}s')
+        passo += 0.1
+
+    for formiga in range(formigas): #loop para cada formiga
+        #formiga inicia de uma cidade aleatória
+        cidadeAtual = random.randint(0, len(coords) -1) 
+        #inicia lista com todas as cidades nao visitadas
+        cidadeNaoVisitada = list(range(qtdCidades))
+        #remove a primeira cidade da lista de cidades não visitadas
+        cidadeNaoVisitada.remove(cidadeAtual)
+        #adiciona a primeira cidade na lista de rota feita
+        rota = [cidadeAtual]
+        #inicializa a distancia total percorrida pela formiga
+        distanciaTotal = 0
+        #loop que vai rodar até a formiga percorrer cada cidade
+        for _ in range(len(cidadeNaoVisitada)):
+            #função probabilidade descrita no artigo do dorigo. esse é apenas o numerador
+            probabilidade = (matrizFeromonio[cidadeAtual, cidadeNaoVisitada] ** alfa) * ((1.0 / matrizDistancia[cidadeAtual, cidadeNaoVisitada]) ** beta)
+            #o denominador da função é o somatório do numerador. 
+            probabilidade = probabilidade / sum(probabilidade)
+            #escolhe a próxima cidade baseada na probabilidade de cada trecho
+            proximaCidade = np.random.choice(cidadeNaoVisitada, p=probabilidade)
+            #adiciona a próxima cidade escolhida na rota da formiga
+            rota.append(proximaCidade)
+            #adiciona a distância percorrida entre a cidade atual e a proxima cidade na variavel de distancia percorrida
+            distanciaTotal += matrizDistancia[cidadeAtual, proximaCidade]
+            #atualiza a cidade atual
+            cidadeAtual = proximaCidade
+            #remove a cidade atual das não visitadas
+            cidadeNaoVisitada.remove(cidadeAtual)
+        #no final da rota, adiciona o ponto inicial
+        rota.append(rota[0])
+        #adiciona a distancia da última cidade até a cidade inicial
+        distanciaTotal += matrizDistancia[cidadeAtual, rota[0]]
+        
+        #armazena a rota feita
+        caminhoFormiga.append(rota)
+        #armazena a distância da rota percorrida
+        caminhoFormigaDistancia.append(distanciaTotal)
+        #verifica se a distancia percorrida pela formiga é a menor global
+        if distanciaTotal < melhorDistancia:
+            #se sim, pega a melhor distancia e melhor rota
+            melhorDistancia = distanciaTotal
+            melhorRota = rota
+    
+    #atualiza a matriz feromonio de acordo com a evaporação
+    matrizFeromonio = matrizFeromonio * (1.0 - rho)
+    for formiga, rota in enumerate(caminhoFormiga):
+        for i in range(qtdCidades):
+            matrizFeromonio[rota[i], rota[i + 1]] += Q / caminhoFormigaDistancia[formiga]
+
+    #pega o indice da melhor rota
+    indiceMelhorRota = np.argmin(caminhoFormigaDistancia)
+    #usa o indice pra pegar a distancia correta
+    melhoresDistancias.append(caminhoFormigaDistancia[indiceMelhorRota])
+    melhoresRotas.append(caminhoFormiga[indiceMelhorRota])
+
+execucao = time.time() - inicio
+print(f"100.0% iteração = {iteracoes} - {execucao:.3f}s")
+
+#cria um array 2d com as coordenadas da melhor rota
+melhorRota_cidades = [cidades[i] for i in melhorRota]
+melhorRota_cidades = np.array(melhorRota_cidades)
+print(melhoresDistancias)
+#imprime a melhor distancia
+print(f'{melhorDistancia:.0f}')
+#plot com as melhores distancias por iteracao
+plotIterDistancias(melhoresDistancias, alfa, beta, rho)
+#plot com o caminho feito
+plotCaminho(melhorRota_cidades, cidades, alfa, beta, rho)
---
+++ 2VA/coloniadeformigas/algoritmogenetico.py
@@ -0,0 +1,129 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math, random, time
+
+def lerEntrada():
+    arquivo = open('berlin52.tsp')    #substituir por matriztsp7.tsp para entradas pequenas e berlin52.tsp para entradas grandes
+    arquivo = arquivo.readlines()       #coloca cada linha em uma lista diferente
+    aux = coords = []
+    for i in range(6, len(arquivo)):    #começa o loop a partir da sexta linha onde começam as coordenadas
+        aux = list(map(float, arquivo[i].split()))  #divide a linha em 3, indice da cidade, x e y
+        coords.append([aux.pop(1), aux.pop(1)])     #remove o x e depois remove o y
+    return coords
+
+def matrizes(coords, qtdCidades):
+    #Iniciar a matriz 52x52 com zeros
+    matrizDistancia = np.zeros((qtdCidades, qtdCidades))
+    for i in range(qtdCidades):
+        for j in range(qtdCidades -1, i, -1):
+            #Calcular cada distancia euclidiana entre as cidades e colocar o resultado na matriz
+            matrizDistancia[i][j] = math.sqrt(((coords[i][0] - coords[j][0])**2) + ((coords[i][1] - coords[j][1])**2))
+            #espelhar a matriz para que as duas metades tenham mesmos valores
+            matrizDistancia[j][i] = matrizDistancia[i][j]
+    return matrizDistancia
+
+def plotIterDistancias(melhoresDistancias, geracoes, tamanho_populacao, taxa_mutacao):
+    #plot das distâncias por geração
+    plt.figure(figsize=(10, 6))
+    plt.plot(melhoresDistancias, marker='o', linestyle='-', color='b')
+    plt.xlabel('Geração')
+    plt.ylabel('Melhor distância')
+    plt.title(f'Melhores distâncias | Gerações: {geracoes}, Tamanho da População : {tamanho_populacao}, Taxa de Mutação: {taxa_mutacao}')
+    plt.grid(True)
+    #plt.show(block=False)
+    plt.savefig("GAdistanciasgeracoes.png")
+
+def plotCaminho(melhorRota_cidades, cidades, geracoes, tamanho_populacao, melhorDistancia, taxa_mutacao):
+    #plot do melhor caminho
+    plt.figure(figsize=(10, 6))
+    plt.scatter(cidades[:, 0], cidades[:, 1], color='blue', label='Cidades')
+    plt.plot(melhorRota_cidades[:, 0], melhorRota_cidades[:, 1], color='red', linewidth=2, label='Rota')
+    for i, cidade in enumerate(melhorRota_cidades):
+        plt.text(cidade[0], cidade[1], str(i), fontsize=12, color='black')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.title(f'Melhor Solução: {melhorDistancia:.2f} | Gerações: {geracoes}, Tamanho da População : {tamanho_populacao}, Taxa de Mutação: {taxa_mutacao}')
+    plt.grid(True)
+    plt.savefig("GAplotCaminho.png")
+
+# Function to generate a random caminho
+def caminhoAleatorio(n):
+    caminho = list(range(n))
+    random.shuffle(caminho)
+    return caminho
+
+# Function to calculate the cost of a caminho
+def fitness(caminho, matriz):
+    custo = 0
+    for i in range(len(caminho) - 1):
+        custo += matriz[caminho[i]][caminho[i + 1]]
+    custo += matriz[caminho[-1]][caminho[0]]
+    return custo
+
+# Function to perform selecao
+def selecao(populacao, matriz):
+    populacao = sorted(populacao, key=lambda x: fitness(x, matriz))
+    aux = len(populacao)//2
+    return populacao[:aux]
+
+# Function to perform crossover
+def crossover(pai1, pai2):
+    geneA = random.randint(0, len(pai1))
+    geneB = random.randint(0, len(pai1))
+    comeco = min(geneA, geneB)
+    fim = max(geneA, geneB)
+    filho = [None] * len(pai1)
+    filho[comeco:fim] = pai1[comeco:fim]
+
+    for cidade in pai2:
+        if cidade not in filho:
+            i = filho.index(None)
+            filho[i] = cidade
+    return filho
+
+# Function to perform mutacao
+def mutacao(caminho, taxa_mutacao):
+    if random.random() < taxa_mutacao:
+        i = random.randint(0, len(caminho) - 1)
+        j = random.randint(0, len(caminho) - 1)
+        caminho[i], caminho[j] = caminho[j], caminho[i]
+    return caminho
+
+# Genetic algorithm function
+def algoritmo_genetico(geracoes, tamanho_populacao, coords, matriz, taxa_mutacao):
+    melhoresDistancias = []
+    populacao = [caminhoAleatorio(len(coords)) for _ in range(tamanho_populacao)]
+    for geracao in range(geracoes):
+        populacao = selecao(populacao, matriz)
+        novapopulacao = []
+        for i in range(tamanho_populacao // 2):
+            pai1, pai2 = random.sample(populacao, 2)
+            filho = crossover(pai1, pai2)
+            novapopulacao.append(mutacao(filho, taxa_mutacao))
+        populacao += novapopulacao
+        melhorCaminho = min(populacao, key=lambda x: fitness(x, matriz))
+        #print(f"Geração {geracao}: Melhor Caminho = {fitness(melhorCaminho, matriz)}")
+        melhoresDistancias.append(fitness(melhorCaminho, matriz))
+    melhorCaminho = min(populacao, key=lambda x: fitness(x, matriz))
+    return melhorCaminho, fitness(melhorCaminho, matriz), melhoresDistancias
+
+def main():
+    inicio = time.time()
+    geracoes = 100  #quantas vezes o algoritmo será rodado
+    tamanho_populacao = 200 #quantos indivíduos serão gerados
+    taxa_mutacao = 0.05 #a probabilidade de um filho sofrer mutação
+    coords = lerEntrada()   # entrada com as coordenadas da cidade
+    cidades = np.array(coords)  #array em 2D com as coordenadas da cidade
+    matriz = matrizes(coords, len(coords))  #matriz distancia
+
+    melhorCaminho, melhorDistancia, melhoresDistancias = algoritmo_genetico(geracoes, tamanho_populacao, coords, matriz, taxa_mutacao)
+    melhorRota_cidades = [cidades[i] for i in melhorCaminho]
+    melhorRota_cidades = np.array(melhorRota_cidades)
+    execucao = time.time() - inicio
+    plotIterDistancias(melhoresDistancias, geracoes, tamanho_populacao, taxa_mutacao)
+    plotCaminho(melhorRota_cidades, cidades, geracoes, tamanho_populacao, melhorDistancia, taxa_mutacao)
+    print(f"Melhor Distancia: {melhorDistancia:.2f}, tempo: {execucao:.3f}s")
+
+if __name__ == "__main__":
+    main()
